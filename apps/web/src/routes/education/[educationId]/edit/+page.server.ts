f16e0def3715130bb1bb75d01737842050f0d6ab
Timothy Capper <timcapper@gmail.com>
2024-09-28 13:05:39 +0100

    Added edit education

---
+++ apps/web/src/routes/education/[educationId]/edit/+page.server.ts
@@ -0,0 +1,40 @@
+import { serializeNonPOJOs } from '$lib/utils.js';
+import { error, redirect } from '@sveltejs/kit';
+
+export const load = async ({ locals, params }) => {
+    if (!locals.pb.authStore.isValid) {
+        throw error(401, 'Unauthorized');
+    }
+
+    try {
+        const education = serializeNonPOJOs(
+            await locals.pb.collection('education').getOne(params.educationId)
+        );
+
+        if (locals.user.id === education.userId) {
+            return {education};
+        } else {
+            throw error(403, 'Forbidden');
+        }
+    } catch (err) {
+        console.log('Error: ', err);
+        throw error(err.status, err.message);
+    }
+};
+
+export const actions = {
+    updateEducation: async ({ request, locals, params }) => {
+        const formData = await request.formData();
+
+        formData.append('userId', locals.user.id);
+
+        try {
+            await locals.pb.collection('education').update(params.educationId, formData);
+        } catch (err) {
+            console.log('Error: ', err);
+            throw error(err.status, err.message);
+        }
+
+        throw redirect(303, `/education/${params.educationId}`);
+    }
+};
---
+++ apps/web/src/routes/education/[educationId]/edit/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { Input } from '$lib/components';
+    export let data;
+</script>
+
+<div class="flex flex-col w-full h-full p-2">
+    <div class="w-full">
+        <form method="POST" action="?/updateEducation" class="flex flex-col space-y-2 w-full items-center">
+            <h3 class="text-3xl font-bold">Edit {data.education.subject} at {data.education.institution}</h3>
+            <Input id="institution" label="Institution" value={data.education.institution ?? ''} />
+            <Input id="subject" label="Subject" value={data.education.subject ?? ''} />
+            <Input id="qualification" label="Qualification" value={data.education.qualification ?? ''} />
+            <Input id="startDate" type="date" label="Start date" value={data.education.startDate ?? ''} />
+            <Input id="endDate" type="date" label="End date" value={data.education.endDate ?? ''} />
+            <Input id="grade" label="Grade" value={data.education.grade ?? ''} />
+            <div class="w-full max-w-lg pt-3">
+                <button type="submit" class="btn btn-primary w-full max-w-lg">Save changes</button>
+            </div>
+        </form>
+    </div>
+</div>
