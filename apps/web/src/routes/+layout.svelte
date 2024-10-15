324026e207c221adc2f52e1f337c08d18d00b6bb
Timothy Capper <timcapper@gmail.com>
2024-10-15 10:28:28 +0100

    Changed to left side bar

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,47 +1,23 @@
 <script>
+	import MynauiCogOne from '~icons/mynaui/cog-one';
+	import MynauiLogoutSolid from '~icons/mynaui/logout-solid';
+
 	import '../app.css';
-	export let data
+
+	export let data;
 </script>
 
+{#if !data.user}
 <div class="min-h-full">
 	<nav class="navbar bg-base-100 border-b">
 		<div class="flex-1">
 			<a href="/" class="btn btn-ghost normal-case text-xl">Showcase</a>
 		</div>
 		<div class="flex-none">
-			{#if !data.user}
-				<div class="dropdown dropdown-end">
-					<a href="/login" class="btn btn-primary">Login</a>
-					<a href="/register" class="btn btn-secondary">Register</a>
-				</div>
-			{:else}
-				<div class="dropdown dropdown-end mr-4">
-					<a href="/projects/new" class="btn btn-primary btn-outline">Add Project</a>
-				</div>
-				<div class="dropdown dropdown-end">
-					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label tabindex="0" class="btn btn-ghost btn-circle avatar">
-						<div class="w-10 rounded-full">
-							<img src="https://placeimg.com/80/80/people" alt="User avatar" />
-						</div>
-					</label>
-					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-					<ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-						<li>
-							<a href="/my/projects" class="justify-between">My Projects</a>
-						</li>
-						<li>
-							<a href="/my/settings/">Settings</a>
-						</li>
-						<li>
-							<form action="/logout" method="POST">
-								<button type="submit" class="w-full text-start">Logout</button>
-							</form>
-						</li>
-					</ul>
-				</div>
-			{/if}
+			<div class="dropdown dropdown-end">
+				<a href="/login" class="btn btn-primary">Login</a>
+				<a href="/register" class="btn btn-secondary">Register</a>
+			</div>
 		</div>
 	</nav>
 	<div class="py-4">
@@ -50,4 +26,23 @@
 		</div>
 	</div>
 </div>
-
+{:else}
+<div class="min-h-screen flex">
+	<nav class="navbar bg-base-100 border-r w-24 h-full flex flex-col p-4">
+		<div class="mb-6">
+			<a href="/" class="btn btn-ghost normal-case text-xl">CV</a>
+		</div>
+		<div class="flex flex-col space-y-4">
+			<a href="/my/settings/" class="btn btn-ghost normal-case text-xl"><MynauiCogOne /></a>
+			<form action="/logout" method="POST">
+				<button class="btn btn-ghost normal-case text-xl" type="submit"><MynauiLogoutSolid /></button>
+			</form>
+		</div>
+	</nav>
+	<div class="flex-grow py-4">
+		<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+			<slot />
+		</div>
+	</div>
+</div>
+{/if}
